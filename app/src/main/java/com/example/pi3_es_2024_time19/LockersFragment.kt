224eea88307366f1d51184e94b8d8374fba40484
euFerro <breno19a.f@gmail.com>
2024-04-20 12:42:47 -0300

    Changed Hrad Coded API_KEY for Google Maps Fragent to local.properties file.

--- app/src/main/java/com/example/pi3_es_2024_time19/LockersFragment.kt
+++ app/src/main/java/com/example/pi3_es_2024_time19/LockersFragment.kt
@@ -7,11 +7,6 @@ import android.view.View
 import android.view.ViewGroup
 import com.example.pi3_es_2024_time19.databinding.FragmentLockersBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
 /**
  * A simple [Fragment] subclass.
  * Use the [LockersFragment.newInstance] factory method to
