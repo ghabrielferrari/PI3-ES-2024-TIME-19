9bf980e6ef1b221e30fad1fcdb8524a14f420203
euFerro <breno19a.f@gmail.com>
2024-04-25 12:32:34 -0300

    Created Recycler view and dynamic rendering for the armario_cliente_list_item.xml

--- app/src/main/java/com/example/pi3_es_2024_time19/AdapterArmarioListItem.kt
+++ app/src/main/java/com/example/pi3_es_2024_time19/AdapterArmarioListItem.kt
@@ -1,48 +1,66 @@
 package com.example.pi3_es_2024_time19
 
+import android.content.res.ColorStateList
+import android.graphics.Color
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.pi3_es_2024_time19.models.Locker
 
 class AdapterArmarioListItem(
-    private val lockers: MutableList<Locker>,
+    private val lockers: MutableList<Locker>
 //    private val action1Listener:
 ) : RecyclerView.Adapter<AdapterArmarioListItem.ArmarioViewHolder>() {
-
-
-    inner class ArmarioViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
-        val tvTitle: TextView = itemView.findViewById(R.id.name)
-        val tvStatus: TextView = itemView.findViewById(R.id.status)
-        val btnAction1: Button = itemView.findViewById(R.id.btnAction1)
-        val btnAction2: Button = itemView.findViewById(R.id.btnAction2)
-
-        init {
-            // quando clicar no botão, lançar outro listener para remover o item.
-            TODO()
-//            ivDeleteItem.setOnClickListener {
-//                listener.delete(items[adapterPosition])
-//            }
-//            checkBox.setOnClickListener {
-//                checkListener.onItemCheck(items[adapterPosition])
-//            }
-        }
-    }
 
     override fun onCreateViewHolder(
         parent: ViewGroup,
         viewType: Int
     ): AdapterArmarioListItem.ArmarioViewHolder {
-        TODO("Not yet implemented")
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.armario_cliente_list_item, parent, false)
+        return ArmarioViewHolder(itemView)
     }
 
     override fun onBindViewHolder(holder: AdapterArmarioListItem.ArmarioViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        val currentListItem = lockers[position]
+        holder.tvTitle.text = currentListItem.name
+        holder.tvStatus.text = currentListItem.status
+        if (currentListItem.isRented == true) {
+//            holder.layoutLogo.setBackgroundColor(Color.parseColor("#00AA00"))
+            holder.tvStatus.setTextColor(Color.parseColor("#00AA00"))
+            holder.btnAction.text = "Info"
+            holder.btnDanger.text = "Cancelar"
+        } else {
+            // Apenas gerente pode ver armários não alugados
+            holder.layoutLogo.setBackgroundColor(Color.parseColor("#FFAA00"))
+            holder.tvStatus.setTextColor(Color.parseColor("#FFAA00"))
+            holder.btnDanger.visibility = View.INVISIBLE
+            holder.btnAction.text = "Liberar"
+        }
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return lockers.size
+    }
+
+    inner class ArmarioViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+        val tvTitle: TextView = itemView.findViewById(R.id.name)
+        val tvStatus: TextView = itemView.findViewById(R.id.status)
+        val btnAction: Button = itemView.findViewById(R.id.btnAction)
+        val btnDanger: Button = itemView.findViewById(R.id.btnDanger)
+        val layoutLogo: LinearLayout = itemView.findViewById(R.id.layoutLocksLogo)
+
+        init {
+//            btnAction.setOnClickListener {
+//                TODO
+//            }
+
+//            btnDanger.setOnClickListener {
+//                TODO
+//            }
+        }
     }
 }
--- app/src/main/java/com/example/pi3_es_2024_time19/fragments/LockersFragment.kt
+++ app/src/main/java/com/example/pi3_es_2024_time19/fragments/LockersFragment.kt
@@ -6,17 +6,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView.LayoutManager
+import com.example.pi3_es_2024_time19.AdapterArmarioListItem
 import com.example.pi3_es_2024_time19.RentLockerActivity
 import com.example.pi3_es_2024_time19.databinding.FragmentLockersBinding
+import com.example.pi3_es_2024_time19.models.Locker
 
-/**
- * A simple [Fragment] subclass.
- * Use the [LockersFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class LockersFragment : Fragment() {
 
     private lateinit var binding: FragmentLockersBinding
+    private lateinit var lockers: MutableList<Locker>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,12 +26,16 @@ class LockersFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
+
         setBinding()
+        setupRecyclerView()
+
         binding.btnAddNovoArmario.setOnClickListener {
-            val intent = Intent(context, RentLockerActivity::class.java)
-            startActivity(intent)
+            openRentLockerActivity()
         }
+
+        setDummyListItems()
+        bindAdapter()
 
         return binding.root
     }
@@ -40,5 +44,31 @@ class LockersFragment : Fragment() {
         binding = FragmentLockersBinding.inflate(layoutInflater)
     }
 
+    private fun openRentLockerActivity() {
+        val intent = Intent(context, RentLockerActivity::class.java)
+        startActivity(intent)
+    }
 
+    private fun setupRecyclerView() {
+        binding.rvArmarios.layoutManager = LinearLayoutManager(context)
+        binding.rvArmarios.setHasFixedSize(true)
+    }
+
+    private fun setDummyListItems() {
+        lockers = mutableListOf()
+        val li1 = Locker("Armário 1", "Alugando", true)
+//        val li2 = Locker("Armário 2", "Alugando", true)
+        val li3 = Locker("Armário 3", "Livre", false)
+//        val li4 = Locker("Armário 4", "Alugando", true)
+
+        lockers.add(li1)
+//        lockers.add(li2)
+        lockers.add(li3)
+//        lockers.add(li4)
+    }
+
+    private fun bindAdapter() {
+        val adapter = AdapterArmarioListItem(lockers)
+        binding.rvArmarios.adapter = adapter
+    }
 }
